d28e8fda1debd003da11c9bb532d8ca571a68fe7
Pedro Bruel <pedro.bruel@gmail.com>
2021-05-11 18:16:52 -0300

    Update ubuntu lts install script

--- install/ubuntu_20042_lts.sh
+++ install/ubuntu_20042_lts.sh
@@ -4,42 +4,42 @@ set -e
 
 function usage() {
     echo "Usage: $0 [OPTION]"
-    echo -e "\t-e, --emacs\tCopy \"init.el\" to user home [$HOME/.emacs.d/]"
-    echo -e "\t-t, --test\tTest user configuration [$HOME/.emacs.d/init.el]"
+    echo -e "\t-i, --install\tInstall dependencies (requires sudo privileges)"
     echo -e "\t-h, --help\tPrint this message"
     exit 0
 }
 
 # http://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
 if [ ${PWD##*/} != "deploy-org" ]
-   then
-   echo -e "\tMust run \"$0\" from repository root: deploy-org"
-   exit -1
+then
+    echo "Running from $PWD"
+    echo "Must run \"$0\" from repository root: deploy-org"
+    exit -1
 fi
 
 if [ $# -eq 0 ]
-   then
-   usage
-   exit
+then
+    usage
+    exit
 fi
 
 while test $# -gt 0
 do
     case "$1" in
-        -e|--emacs)
-            echo "Copying \"init.el\" to $HOME/.emacs.d/"
-
-            if [ ! -d "$HOME/.emacs.d" ]
-            then
-                echo "Making directory"
-                mkdir $HOME/.emacs.d
-            fi
-
-            cp emacs.d/init.el
-
-            ;;
-        -t|--test)
-            echo "Running pdf compilation test"
+        -i|--install)
+            echo "Installing dependencies (requires sudo privileges)"
+            apt-get update
+            apt-get install -y --no-install-recommends \
+                    build-essential \
+                    python3 \
+                    r-base \
+                    julia \
+                    git \
+                    emacs \
+                    texlive-latex-extra \
+                    texlive-xetex \
+                    texlive-luatex \
+                    latexmk
             ;;
         -h|--help|*)
             usage
